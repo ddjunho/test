ee150258ff9d00332f2516ef24d6c6cc997229f0
권준오 <112926563+ddjunho@users.noreply.github.com>
2023-07-19 10:14:00 +0900

    Add files via upload

---
+++ giveandtake/app/src/main/java/com/example/giveandtake/MenuFragment.java
@@ -0,0 +1,24 @@
+package com.example.giveandtake;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MenuFragment extends Fragment {
+
+    public MenuFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_menu, container, false);
+    }
+}
